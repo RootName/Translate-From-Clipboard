80fb48ba8fa7cb0c24cc8835233c637c1dc8b6a1
Black Viking <yakuza3003@yandex.com>
2017-03-12 15:46:02 +0200

    listener.py

--- listener.py
+++ listener.py
@@ -15,38 +15,26 @@ sys.setdefaultencoding("utf-8")
 
 espeak = "espeak -v tr \""
 
-def translate(type):
-	def sendResult(title, message, x):
-		if x == "1":
-			os.system("%s %s"%(espeak, message))
-		elif x == "2":
-			subprocess.call(["notify-send.exe", 'Ceviri: %s'%(title), '%s'%(message.lower().replace("ı", "i").replace("ü", "u").replace("ş", "s").replace("ö", "o").replace("ğ", "g"))])
-
+def translate():
 	translator = Translator(to_lang="tr")
 	f = open('history.txt', 'a')
 	clipboard.copy("")
-	print "\n[+] Listener started!"
 	while True:
 		try:
 			if clipboard.paste():
 				word = clipboard.paste().strip()
 				mean = translator.translate(word)
+				subprocess.call(["notify-send.exe", 'Ceviri: %s'%(word), '%s'%(mean.lower().replace("ı", "i").replace("ü", "u").replace("ş", "s").replace("ö", "o").replace("ğ", "g").replace("ç", "c"))])
 				result =  "\n[>] %s\t: %s\n"%(word, mean) + "="*70
 				f.write(result)
-				print result
 				clipboard.copy("")
-				sendResult(word, mean, type)
 			else:
 				pass
 		except KeyboardInterrupt:
 			f.close()
-			print "\n[*] Detected CTRL+C, press Enter to continue..."
-			raw_input()
-			main()
-
+			sys.exit()
 def main():
-	type = raw_input("[-] 1. Text To Speech\n[-] 2. Notification\n[-] --> ")
-	translate(type) if int(type) < 3 else sys.exit()
+	translate()
 
 if __name__ == "__main__":
 	main()

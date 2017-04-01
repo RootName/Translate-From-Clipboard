c116f04986d9f6d84c1852880ad7fdd21c3273dc
Black Viking <yakuza3003@yandex.com>
2017-04-01 14:14:11 +0300

    run on qpython(android)

--- android/listener.py
+++ android/listener.py
@@ -5,7 +5,7 @@
 
 __author__ = "Black Viking"
 __date__   = "28.03.2017"
-
+   
 import sys, os
 import androidhelper
 
@@ -16,67 +16,66 @@ droid = androidhelper.Android()
 fileName = "/sdcard/history.txt"
 
 try:
-	from translate import Translator
+    from translate import Translator
 except ImportError:
-	print "[-] Çeviri için gerekli olan 'translate' modülü bulunamadı!"
-	if raw_input("[?] Yüklensin mi? [Y/n] ==> ").lower() == "y"
-		module()
-	else:
-		sys.exit()
+    print "[-] Çeviri için gerekli olan 'translate' modülü bulunamadı!"
+    if raw_input("[?] Yüklensin mi? [Y/n] ==> ").lower() == "y":
+        module("translate")
+    else:
+        sys.exit()
 
-def module():
-	os.system(sys.executable+" "+sys.prefix+"/bin/"+"pip install %s"%("translate"))
-	from translate import Translator
+def module(module):
+    os.system(sys.executable+" "+sys.prefix+"/bin/"+"pip install %s"%(module))
+    from translate import Translate
 
 def translate(word):
-	translator = Translator(to_lang="tr")
-	mean = translator.translate(word.strip())
-	droid.makeToast("[>] %s: %s"%(word.strip(), mean))
-	return mean
+    translator = Translator(to_lang="tr")
+    mean = translator.translate(word.strip())
+    droid.makeToast("[>] %s: %s"%(word, mean))
+    return mean
 
 def main():
-	os.system("clear")
-	droid.setClipboard("")
+    os.system("clear")
+    droid.setClipboard("")
 
-	try:
-		file = open(fileName, "a")
-		print "[>] File opened, %s"%(fileName)
-		f = True
-	except Exception as error:
-		print "[!] Error: %s"%(error)
-		f = False
+    try:
+        file = open(fileName, "a")
+        print "[>] File opened, %s"%(fileName)
+        f = True
+    except Exception as error:
+        print "[!] Error: %s"%(error)
+        f = False
 
-	print "[>] Listener started!\n"+"-"*40
-	droid.makeToast("[>] Listener Started!")
+    print "[>] Listener started!\n"+"-"*60
+    droid.makeToast("[>] Listener started!")
 
-	while True:
-		if droid.getClipboard().result != "":
-			word = droid.getClipboard().result
-			droid.setClipboard("")
-			mean = translate(word)
-			if mean:
-				result = "[>] %s: %s"%(word, mean)
-				print result
-				droid.makeToast(result)
-				if f == False:
-					pass
-				else:
-					file.write(result+"\n")
-					file.flush()
-			else:
-				pass
-		else:
-			pass
+    while True:
+        if droid.getClipboard().result != "":
+            word = droid.getClipboard().result
+            droid.setClipboard("")
+            mean = translate(word)
+            if mean:
+                result = "[>] %s : %s"%(word, mean)
+                print result
+                if f == False:
+                    pass
+                else:
+                    file.write(result+"\n")
+                    file.flush()
+            else:
+                pass
+            
+        else:
+            pass
 
 def start():
-	try:
-		main()
-	except Exception as Error:
-		droid.makeToast("[!] ERROR: %s"%(Error))
-		print "[!] ERROR: %s"%(Error)
-		print "-"*40
-		raw_input("Press 'Enter' to continue...")
-		start()
-
+    try:
+        main()
+    except Exception as Error:
+        droid.makeToast("[!] ERROR: %s"%(Error))
+        print "[!] ERROR: %s"%(Error)
+        print "-"*60
+        raw_input("Press 'Enter' to continue...")
+        start()
 if __name__ == "__main__":
-	start()
+    start()
